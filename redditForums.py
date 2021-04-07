9cfaa2712dca53b2d46040b2444b774bd50aa318
Lilly Sharples <lilly.sharples@gmail.com>
2021-04-07 17:27:35 -0400

    all data added to overall dataset

--- redditForums.py
+++ redditForums.py
@@ -20,29 +20,54 @@ import sklearn.metrics as metrics
 
 crypto = pd.read_csv('Data/Crypto.csv', usecols=['body'])
 crypto['forum'] = "r/Crypto"
-wsb = pd.read_csv('Data/WallStreetBets.csv', usecols=['body'])
+
+wsb = pd.read_csv('Data/WallStreetBets.csv', usecols=['title'])
+wsb.rename(columns={'title':'body'}, inplace=True)
 wsb['forum'] = "r/WallStreetBets"
+
+
+#combined android datasets from 2018-2019 and 2019-2020
+android = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/main/data/r_androiddev/androiddev_2019_2020.csv?token=AO4LMYPMCJ3WB6Q3BEE576TAO4ECE', usecols=['text'])
+android.rename(columns={'text':'body'}, inplace=True)
+android2 = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/main/data/r_androiddev/androiddev_2018_2019.csv?token=AO4LMYOSRHE2ZXWQYYJNYETAO5IOS', usecols=['text'])
+android2.rename(columns={'text':'body'}, inplace=True)
+android = pd.concat([android, android2])
+android['forum'] = 'r/AndroidDev'
+
+news = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/main/data/reddit_worldnews_start_to_2016-11-22.csv?token=AO4LMYIOO3E7K25BLB7YZVTAO5I44', usecols=['title'])
+news.rename(columns={'title':'body'}, inplace=True)
+news['forum'] = 'r/WorldNews'
+
+#maybe combine vaccine myths and coronavirus since they are both small?
 vm = pd.read_csv('Data/VaccineMyths.csv', usecols=['body'])
 vm['forum'] = "r/VaccineMyths"
-#data = crypto.append(wsb)
-#data.append(vm)
-data = pd.DataFrame().append([crypto,wsb,vm])
+cv = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/main/data/Coronavirus_1k.csv?token=AO4LMYKDHGLUO2LD2THDWGTAO4CRI', usecols=['title'])
+cv.rename(columns={'title':'body'}, inplace=True)
+cv['forum'] = "r/Coronavirus"
+
+
+data = pd.DataFrame().append([crypto,wsb,vm,cv,android,news,trump])
+#get rid of null entries
+print(data['forum'].value_counts())
 df = data.dropna()
 print(df['forum'].value_counts())
 
 count_vect = CountVectorizer()
 train, test = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
+#print(train['forum'].value_counts())
+#print(test['forum'].value_counts())
 X_train = count_vect.fit_transform(train.body)
 X_test = count_vect.transform(test.body)
 Y_train = train.forum
 Y_test = test.forum
 
 
-#print(train['forum'].value_counts())
-
-
-#X_train_counts = count_vect.fit_transform(twenty_train.data)
-
+#original accuracy: MNB .965, BNB .859
+print('before smote')
+from imblearn.over_sampling import SMOTE
+sm = SMOTE()
+X_train, Y_train = sm.fit_resample(X_train, Y_train)
+print('after smote')
 clfM = MultinomialNB().fit(X_train, Y_train)
 clfB = BernoulliNB().fit(X_train, Y_train)
 
