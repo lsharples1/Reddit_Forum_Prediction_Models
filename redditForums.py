2c3bd5713e547b4d5af38efcd396c0ff51d23c40
Lilly Sharples <62436961+lsharples1@users.noreply.github.com>
2021-04-12 22:28:17 -0400

    Add files via upload
    Removed SMOTE. Added comments

--- redditForums.py
+++ redditForums.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import os
 import numpy as np
 import pandas as pd
@@ -21,7 +20,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 
 crypto = pd.read_csv('Data/Crypto.csv', usecols=['body'])
-crypto['forum'] = "r/Crypto" #2018.01
+crypto['forum'] = "r/Crypto"
 
 wsb = pd.read_csv('Data/WallStreetBets.csv', usecols=['title'])
 wsb.rename(columns={'title':'body'}, inplace=True)
@@ -39,82 +38,85 @@ news = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/
 news.rename(columns={'title':'body'}, inplace=True)
 news['forum'] = 'r/WorldNews'
 
-#maybe combine vaccine myths and coronavirus since they are both small?
 vm = pd.read_csv('Data/VaccineMyths.csv', usecols=['body'])
 vm['forum'] = "r/VaccineMyths"
+
 cv = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/main/data/Coronavirus_1k.csv?token=AO4LMYKDHGLUO2LD2THDWGTAO4CRI', usecols=['title'])
 cv.rename(columns={'title':'body'}, inplace=True)
 cv['forum'] = "r/Coronavirus"
 
-data = pd.DataFrame().append([wsb,vm,cv,android,news])
-#get rid of null entries
-#print(data['forum'].value_counts())
-df = data.dropna()
-#print(df['forum'].value_counts())
-print(df.head)
 
-train, test = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
-#print(train['forum'].value_counts())
-#print(test['forum'].value_counts())
+data = pd.DataFrame().append([crypto,wsb,vm,cv,android,news])
+df = data.dropna()
 count_vect = CountVectorizer()
+
+#data default split .25/.75
+train, test = train_test_split(df, random_state=5, shuffle=True)
+
 X_train = count_vect.fit_transform(train.body)
 X_test = count_vect.transform(test.body)
+
+Y_train = train.forum
+Y_test = test.forum
 
 tfidf_count_vect = TfidfVectorizer()
 tfidf_X_train = tfidf_count_vect.fit_transform(train.body)
 tfidf_X_test = tfidf_count_vect.transform(test.body)
 
-Y_train = train.forum
-Y_test = test.forum
-
-#original accuracy: MNB .965, BNB .859, TFIDF .977
-print('after smote')
-from imblearn.over_sampling import SMOTE
-sm = SMOTE()
-
-# multinomial and bernoulli models
-X_train, Y_train = sm.fit_resample(X_train, Y_train)
+#MNB .965, BNM .860, RS =  5, default train test split
 clfM = MultinomialNB().fit(X_train, Y_train)
 clfB = BernoulliNB().fit(X_train, Y_train)
 
+Y_predM = clfM.predict(X_test)
+Y_predB = clfB.predict(X_test)
+
+
 # tfidf model
-Y_train = Y_train[0:455600]
-tfidf_X_train, Y_train = sm.fit_resample(tfidf_X_train, Y_train)
-clfT = BernoulliNB().fit(tfidf_X_train, Y_train)
+clfTB = BernoulliNB().fit(tfidf_X_train, Y_train)
+clfTM = MultinomialNB().fit(tfidf_X_train, Y_train)
 
 # predictions and accuracies
 
 Y_predM = clfM.predict(X_test)
 Y_predB = clfB.predict(X_test)
-Y_predT = clfT.predict(tfidf_X_test)
+Y_predTB = clfTB.predict(tfidf_X_test)
+Y_predTM = clfTB.predict(tfidf_X_test)
 
 accuracyM = metrics.accuracy_score(Y_test, Y_predM)
 accuracyB = metrics.accuracy_score(Y_test, Y_predB)
-accuracyT = metrics.accuracy_score(Y_test, Y_predT)
+accuracyTB = metrics.accuracy_score(Y_test, Y_predTB)
+accuracyTM = metrics.accuracy_score(Y_test, Y_predTM)
 
-# plot
 
-# ['WallStreetBets','VaccineMyths','Coronavirus','Android','World News']
+
+accuracyM = metrics.accuracy_score(Y_test, Y_predM)
+accuracyB = metrics.accuracy_score(Y_test, Y_predB)
+
+
 def plot(classifier, x, y, title): 
-    class_names = ['WSB','VM','C','A','WN']
+    class_names = ['Crypto','WSB','VM','Corona','Android','WN']
     disp = plot_confusion_matrix(classifier, x, y,
                                  display_labels=class_names,
-                                 cmap=plt.cm.PuRd
+                                 cmap=plt.cm.PuRd,values_format = ''
                                    )
     disp.ax_.set_title(title)
     plt.show() 
     
 #Multinomial NB with no adjustments
 plot(clfM, X_test, Y_predM, "Multinomial NB")
-print('Multinomial Accuracy (OG data) = ' +str(accuracyM))
+print('Multinomial Accuracy = ' +str(accuracyM))
 
 #Bernoulli NB with no adjustments
 plot(clfB, X_test, Y_predM, "Bernoulli NB")
-print('Bernoulli Accuracy (OG data) = ' + str(accuracyB))
+print('Bernoulli Accuracy = ' + str(accuracyB))
 
 #TFIDF NB with no adjustments
-plot(clfT, X_test, Y_predM, "TFIDF NB")
-print('TFIDF Accuracy (OG data) = ' + str(accuracyT))
+plot(clfTB, X_test, Y_predM, "TFIDF Bernoulli NB")
+print('TFIDF Bernoulli Accuracy = ' + str(accuracyTB))
+
+#TFIDF NB with no adjustments
+plot(clfTM, X_test, Y_predM, "TFIDF Multinomial NB")
+print('TFIDF Multinomial Accuracy = ' + str(accuracyTM))
 
 #X_new_counts = count_vect.transform(test.body)
 #print(X_new_counts)
