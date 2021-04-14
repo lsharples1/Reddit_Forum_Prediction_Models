24da212c8528e1a5a067fe6c102dc6bfadcc80ac
kimberly-nguyen <43930484+kimberly-nguyen@users.noreply.github.com>
2021-04-14 00:04:39 -0400

    fixed matrices and some other errors

--- redditForums.py
+++ redditForums.py
@@ -7,7 +7,7 @@ from IPython.display import display
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
-#nltk.download('stopwords')
+nltk.download('stopwords')
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn import preprocessing
@@ -45,7 +45,6 @@ cv = pd.read_csv('https://raw.githubusercontent.com/lsharples1/Mini_Project_2/ma
 cv.rename(columns={'title':'body'}, inplace=True)
 cv['forum'] = "r/Coronavirus"
 
-
 data = pd.DataFrame().append([crypto,wsb,vm,cv,android,news])
 df = data.dropna()
 
@@ -53,51 +52,36 @@ df = data.dropna()
 stop = stopwords.words('english')
 df['without_stopwords'] = df['body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
 
-
-count_vect = CountVectorizer()
-
 #data default split .25/.75
 train, test = train_test_split(df, random_state=5, shuffle=True)
 
+count_vect = CountVectorizer()
 X_train = count_vect.fit_transform(train.without_stopwords)
 X_test = count_vect.transform(test.without_stopwords)
-
-Y_train = train.forum
-Y_test = test.forum
 
 tfidf_count_vect = TfidfVectorizer()
 tfidf_X_train = tfidf_count_vect.fit_transform(train.without_stopwords)
 tfidf_X_test = tfidf_count_vect.transform(test.without_stopwords)
 
+Y_train = train.forum
+Y_test = test.forum
+
 #MNB .965, BNM .860, RS =  5, default train test split
 clfM = MultinomialNB().fit(X_train, Y_train)
 clfB = BernoulliNB().fit(X_train, Y_train)
 
-Y_predM = clfM.predict(X_test)
-Y_predB = clfB.predict(X_test)
-
-
 # tfidf model
-clfTB = BernoulliNB().fit(tfidf_X_train, Y_train)
 clfTM = MultinomialNB().fit(tfidf_X_train, Y_train)
 
 # predictions and accuracies
 
 Y_predM = clfM.predict(X_test)
 Y_predB = clfB.predict(X_test)
-Y_predTB = clfTB.predict(tfidf_X_test)
-Y_predTM = clfTB.predict(tfidf_X_test)
+Y_predTM = clfTM.predict(tfidf_X_test)
 
 accuracyM = metrics.accuracy_score(Y_test, Y_predM)
 accuracyB = metrics.accuracy_score(Y_test, Y_predB)
-accuracyTB = metrics.accuracy_score(Y_test, Y_predTB)
 accuracyTM = metrics.accuracy_score(Y_test, Y_predTM)
-
-
-
-accuracyM = metrics.accuracy_score(Y_test, Y_predM)
-accuracyB = metrics.accuracy_score(Y_test, Y_predB)
-
 
 def plot(classifier, x, y, title): 
     class_names = ['Crypto','WSB','VM','Corona','Android','WN']
@@ -109,24 +93,14 @@ def plot(classifier, x, y, title):
     plt.show() 
     
 #Multinomial NB with no adjustments
-plot(clfM, X_test, Y_predM, "Multinomial NB")
+plot(clfM, X_test, Y_test, "Multinomial NB")
 print('Multinomial Accuracy = ' +str(accuracyM))
 
 #Bernoulli NB with no adjustments
-plot(clfB, X_test, Y_predM, "Bernoulli NB")
+plot(clfB, X_test, Y_test, "Bernoulli NB")
 print('Bernoulli Accuracy = ' + str(accuracyB))
 
 #TFIDF NB with no adjustments
-plot(clfTB, X_test, Y_predM, "TFIDF Bernoulli NB")
-print('TFIDF Bernoulli Accuracy = ' + str(accuracyTB))
-
-#TFIDF NB with no adjustments
-plot(clfTM, X_test, Y_predM, "TFIDF Multinomial NB")
+plot(clfTM, X_test, Y_test, "TFIDF Multinomial NB")
 print('TFIDF Multinomial Accuracy = ' + str(accuracyTM))
 
-#X_new_counts = count_vect.transform(test.body)
-#print(X_new_counts)
-
-#predicted = clfM.predict(X_new_counts)
-#for doc, category in zip(test.body, predicted):
-   # print(f'{doc} => {category}')
